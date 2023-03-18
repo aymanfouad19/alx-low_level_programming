a4d74829df2c133eb385369169d89f4a112b053b
fouadayman <aymanwacwac@outlook.fr>
2023-03-18 09:38:55 -0700

    nice fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(NULL));
 n = rand() % 3;
 if (n == 0)
 {
-printf("0 is zero");
+printf("0 is zero\n");
 }
 else if (n == 1)
 {
-printf("98 is positive");
+printf("98 is positive\n");
 }
 else
 {
-printf("-98 is negative");
+printf("-98 is negative\n");
 }
 return (0);
 }
