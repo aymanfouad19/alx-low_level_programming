cbbaa3e848fd2d78759bbdc4c9bdff5efc57ff41
fouadayman <aymanwacwac@outlook.fr>
2023-03-18 05:12:21 -0700

    nice fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n
 ssize_t len = 0;
 while (msg[len] != '\0')
 len++;
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+write(2, msg, len);
 return (1);
 }
