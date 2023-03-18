19c26c209127ca74a3fba5a71b318c02749b5cf2
fouadayman <aymanwacwac@outlook.fr>
2023-03-18 05:23:57 -0700

    nice fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -6,10 +7,12 @@
  */
 int main(void)
 {
-const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-ssize_t len = 0;
-while (msg[len] != '\0')
-len++;
-write(STDERR_FILENO, msg, len);
-return (1);
+    const char *msg = "and that piece of art is useful\""
+                      " - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 0;
+
+    while (msg[len] != '\0')
+        len++;
+    write(STDERR_FILENO, msg, len);
+    return (1);
 }
