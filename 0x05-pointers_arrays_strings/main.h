17c493db9d0c16f786f7f26083c31aa819482530
fouadayman <aymanwacwac@outlook.fr>
2023-03-27 14:37:57 -0700

    nice fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
-int _putchar(char c);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
+void _putchar(char c);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
-int _atoi(char *s);
 #endif /* MAIN_H */
