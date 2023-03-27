be32fbee2a1b980821234c1f6257e197522f9788
fouadayman <aymanwacwac@outlook.fr>
2023-03-27 07:17:39 -0700

    nicefix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 int _putchar(char c);
-int _strlen(char *s);
 void _puts(char *str);
+int _strlen(char *s);
 void print_rev(char *s);
+void rev_string(char *s);
 #endif /* MAIN_H */
