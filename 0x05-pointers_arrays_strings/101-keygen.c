0bcffdf040fc653eac2c7c016bd16d60f0ae5a96
fouadayman <aymanwacwac@outlook.fr>
2023-03-27 15:18:04 -0700

    nice fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define PASSWORD_LEN 26
 int main(void)
 {
-int password[4];
-int sum = 0x5a4;
+char password[PASSWORD_LEN + 1]; // +1 for null terminator
 int i;
-for (i = 0; i < 4; i++) {
-password[i] = rand() % 57 + 1; // Random integer between 1 and 57 inclusive
-sum -= password[i]; // Subtract integer from sum
+srand(time(NULL)); // initialize random number generator
+for (i = 0; i < PASSWORD_LEN; i++) {
+password[i] = rand() % 94 + 33; // generate random printable ASCII character
 }
-if (sum == 0) {
-printf("%c%c%c%c\n", password[0], password[1], password[2], password[3]);
-} else {
-main();
-}
+password[PASSWORD_LEN] = '\0'; // add null terminator
+printf("%s\n", password); // print password
 return 0;
 }
