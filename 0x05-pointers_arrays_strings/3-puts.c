ad9a446aefc4705c1a0726acbe31b896cc9edd65
fouadayman <aymanwacwac@outlook.fr>
2023-03-27 07:07:27 -0700

    nicefix

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - Prints a string to stdout followed by a new line
+ * @str: The string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
