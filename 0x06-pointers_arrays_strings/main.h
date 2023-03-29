cd33829c6e79ac955c89b08d6a32e9c6fc6297c1
fouadayman <aymanwacwac@outlook.fr>
2023-03-29 09:46:38 -0700

    nice fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,19 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-/* function prototypes */
-void _puts(char *str);
-int _putchar(char c);
-int _strlen(char *s);
-void _memset(char *s, char b, unsigned int n);
-void _memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-void print_array(int *a, int n);
-void reverse_array(int *a, int n);
+char *string_toupper(char *);
 #endif /* MAIN_H */
