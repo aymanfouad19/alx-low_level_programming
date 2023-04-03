33436ee96f18e0c99e30484deda1e0043e352906
fouadayman <aymanwacwac@outlook.fr>
2023-04-03 03:39:50 -0700

    nice fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* function prototypes */
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+#include <unistd.h>
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(int n);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+char *_strcpy(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 #endif /* MAIN_H */
