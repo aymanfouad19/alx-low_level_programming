5edf1a61a42ba1872509db450ff6f61e2a3226c4
fouadayman <aymanwacwac@outlook.fr>
2023-04-03 03:24:29 -0700

    nice fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,12 @@
-/* main.h */
 #ifndef MAIN_H
 #define MAIN_H
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 #endif /* MAIN_H */
